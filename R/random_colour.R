16d295f6cfc45a186d9298e7c79d647648b5a02b
Martina Starc <martina.starc@outfit7.com>
2021-02-06 19:13:55 +0100

    Added create_question and transparent picture

--- R/random_colour.R
+++ R/random_colour.R
@@ -20,7 +20,7 @@ process_colors <- function(data) {
     mutate(avg = grDevices::rgb(mean(r), mean(g), mean(b), maxColorValue = 255))
 }
 
-quiz_setup <- function(questions, participants, presence = participants$person) {
+quiz_setup <- function(questions, participants, presence = participants$name) {
   quiz <- list()
   quiz$all_questions <- questions %>%
     shuffle_answers()
@@ -29,7 +29,96 @@ quiz_setup <- function(questions, participants, presence = participants$person) 
     shuffle_questions()
   quiz$participants <- participants %>%
     process_colors()
-  quiz$asked <- list()
   quiz$answers <- list()
+  quiz <<- quiz
+}
+
+
+create_question <- function(n) {
+  question <- quiz$questions[n, ] %>% as.list
+  bgcolor <- gplots::col2hex(with(quiz$participants, color[which(name == question$person)]))
+  html_doc <- with(question, glue::glue('
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+html, body {{
+  height: 100%;
+  margin: 0;
+  padding: 20px;
+}}
+
+#question img {{
+  padding: 0;
+  padding-top: 10px;
+  display: block;
+  margin: 0 auto;
+  max-height: 100%;
+  max-width: 100%;
+}}
+
+#question {{
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}}
+
+#question a {{
+  font-size: 50px;
+}}
+
+#question td {{
+  vertical-align: top;
+  text-align: left;
+  width: 25%;
+  padding: 8px;
+  font-size: 20px;
+}}
+
+#question td.q {{
+  font-size: 25px;
+  background-color: #f3f3f3;
+  font-style: normal;
+  padding-bottom: 30px;
+}}
+
+#question tr:nth-child(even){{background-color: white;}}
+
+#question tr:hover {{background-color: transparent;}}
+</style>
+</head>
+<body>
+
+<table id="question">
+  <tr>
+    <td colspan = 4, style = "background-color: {bgcolor}"></td>
+  </tr>
+  <tr>
+    <td class = "q", colspan = 4>{n}. {text}</td>
+  </tr>
+  <tr>
+    <td>A: {answer_A}<img src = "{image_A}"></img></td>
+    <td>B: {answer_B}<img src = "{image_B}"></img></td>
+    <td>C: {answer_C}<img src = "{image_C}"></img></td>
+    <td>D: {answer_D}<img src = "{image_D}"></img></td>
+  </tr>
+  <tr>
+    <td colspan = 4><img src = "a{n}.png", onerror = "this.onerror=null; this.src=\'./inst/pics/transparent.png\'"></img></td>
+  </tr>
+  <tr>
+    <td colspan = 2><a href = "q{n-1}.html"><</a></td>
+    <td colspan = 2, style = "text-align: right"><a href = "q{n+1}.html">></a></td>
+  </tr>
+
+</table>
+
+</body>
+</html>
+'))
+
+  html_file <- file(paste0("q", n, ".html"))
+  writeLines(html_doc, html_file)
+  close(html_file)
+
 }
 
