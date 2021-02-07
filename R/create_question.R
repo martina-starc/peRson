15ecd91c51763c4df382c0787c0b7b63ef441307
Martina Starc <martina.starc@outfit7.com>
2021-02-07 22:06:39 +0100

    Corrected HTML styling, moved styles.css to a css folder, corrected references to it with system.file

--- R/create_question.R
+++ R/create_question.R
@@ -3,12 +3,12 @@ create_question <- function(n) {
   bg_previous <- ifelse(n == 1, quiz$named_colors[["avg"]], quiz$named_colors[unlist(quiz$questions[n - 1, "person"])])
   bg_current <- quiz$named_colors[question$person]
   bg_next <- ifelse(n == nrow(quiz$questions), quiz$named_colors[["avg"]], quiz$named_colors[unlist(quiz$questions[n + 1, "person"])])
-
+  css_file <- system.file("css", "styles.css", package = "peRson")
   html_doc <- with(question, glue::glue('
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="./inst/styles.css">
+<link rel="stylesheet" href="{css_file}">
 <style>
 a.previous:hover {{
   background-color: {bg_previous};
@@ -17,35 +17,33 @@ a.previous:hover {{
 a.next:hover {{
   background-color: {bg_next};
 }}
-
 </style>
 </head>
 <body>
-
 <table id="question">
   <tr>
-    <td colspan = 4, style = "background-color: {bg_current}"></td>
+    <td colspan=4 style="background-color: {bg_current}"></td>
   </tr>
   <tr>
-    <td class = "q", colspan = 4>{n}. {text}</td>
+    <td class="q" colspan=4>{n}. {text}</td>
   </tr>
   <tr>
-    <td>A: {answer_A}<img src = "{image_A}"></img></td>
-    <td>B: {answer_B}<img src = "{image_B}"></img></td>
-    <td>C: {answer_C}<img src = "{image_C}"></img></td>
-    <td>D: {answer_D}<img src = "{image_D}"></img></td>
+    <td>A: {answer_A}<img src="{image_A}"></img></td>
+    <td>B: {answer_B}<img src="{image_B}"></img></td>
+    <td>C: {answer_C}<img src="{image_C}"></img></td>
+    <td>D: {answer_D}<img src="{image_D}"></img></td>
   </tr>
   <tr>
-    <td colspan = 4><img src = "a{n}.png", onerror = "this.onerror=null; this.src=\'./inst/pics/transparent.png\'"></img></td>
+    <td colspan=4><img src="a{n}.png" onerror="this.onerror=null; this.src=\'./inst/pics/transparent.png\'"></img></td>
   </tr>
   <tr>
-    <td colspan = 4><img src = "l{n}.png", onerror = "this.onerror=null; this.src=\'./inst/pics/transparent.png\'"></img></td>
+    <td colspan=4><img src="l{n}.png" onerror="this.onerror=null; this.src=\'./inst/pics/transparent.png\'"></img></td>
   </tr>
 </table>
-<table style = "width: 100%">
+<table id="navigation">
   <tr>
-    <td style = "width: 50%"><a class = "previous", href = "q{n-1}.html"></a></td>
-    <td style = "width: 50%; text-align: right"><a class = "next", href = "q{n+1}.html"></a></td>
+    <td class="previous"><a class="previous" href="q{n-1}.html"></a></td>
+    <td class="next"><a class="next" href="q{n+1}.html"></a></td>
   </tr>
 </table>
 </body>
--- R/show_contestants.R
+++ R/show_contestants.R
@@ -2,6 +2,7 @@ show_contestants <- function(presence, n_per_row = 5) {
 
   bg_next <- quiz$named_colors[unlist(quiz$questions[1, "person"])]
   pic_file <- system.file("pics", "caty_pexels-kelvin-valerio-617278.jpg", package = "peRson")
+  css_file <- system.file("css", "styles.css", package = "peRson")
 
   person_tables <- quiz$participants %>%
     filter(name %in% presence) %>%
@@ -9,13 +10,13 @@ show_contestants <- function(presence, n_per_row = 5) {
     arrange(sample(row_number())) %>%
     purrr::pmap(function(name, image_file, hex, ...) {
       glue::glue('
-<td id = "contestant-wrapper", style = "width: {100 / n_per_row}%">
-<table id = "contestant">
+<td style="width: {100 / n_per_row}%">
+<table class="contestant">
   <tr>
-    <td style = "background-color: {hex}; height: 10px"></td>
+    <td style="background-color: {hex}; height: 10px"></td>
   </tr>
   <tr>
-    <td><img src = "{image_file}"></img></td>
+    <td><img src="{image_file}"></img></td>
   </tr>
   <tr>
     <td>{name}</td>
@@ -27,16 +28,16 @@ show_contestants <- function(presence, n_per_row = 5) {
 
   html_doc <- purrr::map(1:(length(person_tables) %/% n_per_row + ifelse(length(person_tables) %% n_per_row > 0, 1, 0)), ~person_tables[(. * n_per_row - n_per_row + 1):(. * n_per_row)]) %>%
     purrr::map(~paste(unlist(.), collapse = "")) %>%
-    purrr::map(~paste0('<tr style = "background-color: white">', ., "</tr>\n")) %>%
+    purrr::map(~paste0('<tr>', ., "</tr>\n")) %>%
     unlist() %>%
     paste(collapse = "") %>%
     glue::glue('
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="./inst/styles.css">
+<link rel="stylesheet" href="{css_file}">
 <style>
-table {{}
+.contestant-wrapper {{
   width: {200 * n_per_row + 20 * (n_per_row - 1)}px;
   height: {ceiling(length(person_tables) / n_per_row) * 250}px
 }}
@@ -47,14 +48,14 @@ a.next:hover {{
 </style>
 </head>
 <body>
-<table>
-  {tables}
+<table class="contestant-wrapper">
+  {rows}
   <tr>
-    <td colspan = {n_per_row}, style = "width: 100%"><a class = "next", href = "q1.html"></a></td>
+    <td colspan={n_per_row} style = "width: 100%"><a class="next" href="q1.html"></a></td>
   </tr>
 </table>
 </body>
-</html>', tables = .)
+</html>', rows = .)
 
   html_file <- file(paste0("q0.html"))
   writeLines(html_doc, html_file)
